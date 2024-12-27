df1b4f9f36fe77892d3c52fd58cf292610811a79
Axolotls7 <rhysforsberg@gmail.com>
2024-12-27 07:33:13 -0800

    Update Messenger.py

--- Messenger/Messenger.py
+++ Messenger/Messenger.py
@@ -1,15 +1,25 @@
 #This is a message-sending program
 #It isn't good because it doesn't have a server and also some other things
 #But if you really want to, you could do something with this
-peeps = {}
-messages = []
+U = open("Users.txt", "w")
+M = open("Messages.txt", "w")
+peeps = dict(read(open("Users.txt")))
+messages = list(read(open("Messages.txt")))
 a = input("""Password
 
 If new type new and trust the proccess""")
 if a == "new":
-    peeps.update({input(Password):input(Username)})
+    peeps.update(
+        {chr(ord(input(Password))-2):input(Username)})
 else:
     try:
-        print(peeps[a])
+        print(peeps[chr(ord(a)-2)])
     except(keyError):
         quit()
+while True:
+    a = input()
+    if a == "//quit":
+        break
+    else messages.append(a)
+U.write(messages)
+M.write(peeps)
